e5b7ba8d0370c7b4a5a25e8574ec32863eb8fb70
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-09-04 09:22:21 +0500

    work updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
     <>
     <h1 className='text-center text-3xl mt-[20px]'>Olx Product Cards</h1>
     <div className='flex flex-wrap gap-[50px] mt-[20px] justify-center'>
-<Card title="Bata Shoes" description="best qualtiy shoes for men" image="src\Images\shoes.avif"/>
-<Card title="Range Rover" description="best for mounatins and city" image="src\Images\mountain-bike.avif"/>
-<Card title="Mountain Bike" description="best for hills and forests" image="src\Images\range-rover.avif"/>
+<Card title="Bata Shoes" description="best qualtiy shoes for men" image="src/Images/shoes.avif"/>
+<Card title="Range Rover" description="best for mounatins and city" image="src/Images/range-rover.avif"/>
+<Card title="Mountain Bike" description="best for hills and forests" image="src/Images/mountain-bike.avif"/>
 
 
     </div>
